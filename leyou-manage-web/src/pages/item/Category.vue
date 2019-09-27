223e07231fcf543a8a2d68361dac218e9f83560d
kang,chih.lun <kangchihlun@gmail.com>
2019-09-27 17:12:02 +0800

    leyou-web

---
+++ leyou-manage-web/src/pages/item/Category.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <v-tree url="/item/category/list"
+
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+    <!-- :treeData="treeData"指定的話會有預設的樹狀結構-->
+    <!--真實情況是要用url遠程請求獲取數據-->
+  </v-card>
+</template>
+
+<script>
+  import {treeData} from "../../mockDB"
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true,
+        treeData
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+      },
+      handleEdit(id, name) {
+        console.log("edit... id: " + id + ", name: " + name)
+      },
+      handleDelete(id) {
+        console.log("delete ... " + id)
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
