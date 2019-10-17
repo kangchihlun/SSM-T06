bae71bc19eb5fb85a11158c037116cef3e8d89a3
kang,chih.lun <kangchihlun@gmail.com>
2019-10-17 17:19:49 +0800

    upload service 改成使用 fastdfs Tofix : fastdfs  找不到节点或文件

---
+++ leyou/leyou-upload/src/main/java/com/leyou/upload/config/FastClientImporter.java
@@ -0,0 +1,15 @@
+package com.leyou.upload.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题，如果已存在就使用已存在的
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastClientImporter {
+
+}
